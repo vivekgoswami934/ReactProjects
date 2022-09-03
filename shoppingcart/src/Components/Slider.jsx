6db07f076bb36e9c4415969020990291d441d14a
Vivek Goswami <101577915+vivekgoswami934@users.noreply.github.com>
2022-09-03 10:24:45 +0530

    sliding completed

---
+++ shoppingcart/src/Components/Slider.jsx
@@ -0,0 +1,120 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
+import { useState } from "react";
+import styled from "styled-components";
+import { sliderItems } from "../data";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: #eaf0f5;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+  background-color: lightgray;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.7;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.sliderIndex * -100}vw);
+`;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImageContainer = styled.div`
+  height: 94%;
+  flex: 1;
+  /* border: 2px solid teal; */
+  
+  margin-left: 180px;
+`;
+const Image = styled.img`
+  height: 90%;
+  
+
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+  color: teal;
+`;
+const Description = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Slider = () => {
+  const [sliderIndex, setSliderIndex] = useState(0);
+  const handleClick = (direction) => {
+    console.log(direction == "left" ? "left" : "right");
+
+    if (direction == "left") {
+      setSliderIndex(sliderIndex > 0 ? sliderIndex - 1 : 2);
+    } else {
+      setSliderIndex(sliderIndex < 2 ? sliderIndex + 1 : 0);
+    }
+  };
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper sliderIndex={sliderIndex}>
+        {sliderItems.map((item) => {
+          return (
+            <Slide key={item.id * Math.random()}>
+              <ImageContainer>
+                <Image src={item.img} />
+              </ImageContainer>
+              <InfoContainer>
+                <Title> {item.title}</Title>
+                <Description>{item.desc}</Description>
+                <Button>SHOW NOW</Button>
+              </InfoContainer>
+            </Slide>
+          );
+        })}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
--- shoppingcart/src/pages/Home.jsx
+++ shoppingcart/src/pages/Home.jsx
@@ -1,12 +1,15 @@
+
 import React from "react";
 import Announcements from "../Components/Announcements";
 import Navbar from "../Components/Navbar";
+import Slider from "../Components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Announcements />
       <Navbar />
+      <Slider />
     </div>
   );
 };
