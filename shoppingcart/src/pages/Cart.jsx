3cd3363647600607de96790fe1b89aa3583bec15
Vivek Goswami <101577915+vivekgoswami934@users.noreply.github.com>
2022-09-17 08:46:17 +0530

    cart page half

--- shoppingcart/src/pages/Cart.jsx
+++ shoppingcart/src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 import Announcements from "../Components/Announcements";
@@ -22,7 +23,65 @@ const Cart = () => {
           <TopButton type="filled">CHECKOUT NOW</TopButton>
         </Top>
         <Bottom>
-          <Info>Info</Info>
+          <Info>
+            <Product>
+              <ProductDetails>
+                <Image src="https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png" />
+                <Details>
+                  <ProductName>
+                    <b>Prouct:</b>
+                    abcdsesdfnadjf
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 4584578529
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductColor color="red" />
+                  <ProductColor color="teal" />
+                  <ProductSize>
+                    <b>Size:</b> 37.8
+                  </ProductSize>
+                </Details>
+                <PriceDetails>
+                  <ProductAmountContainer>
+                    <Add />
+                    <ProductAmount>2</ProductAmount>
+                    <Remove />
+                  </ProductAmountContainer>
+                  <ProductPrice>$40</ProductPrice>
+                </PriceDetails>
+              </ProductDetails>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetails>
+                <Image src="https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png" />
+                <Details>
+                  <ProductName>
+                    <b>Prouct:</b>
+                    abcdsesdfnadjf
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 4584578529
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductColor color="red" />
+                  <ProductColor color="teal" />
+                  <ProductSize>
+                    <b>Size:</b> 37.8
+                  </ProductSize>
+                </Details>
+                <PriceDetails>
+                  <ProductAmountContainer>
+                    <Add />
+                    <ProductAmount>2</ProductAmount>
+                    <Remove />
+                  </ProductAmountContainer>
+                  <ProductPrice>$40</ProductPrice>
+                </PriceDetails>
+              </ProductDetails>
+            </Product>
+          </Info>
           <Summary>Summary</Summary>
         </Bottom>
       </Wrapper>
@@ -59,6 +118,7 @@ const TopButton = styled.button`
   background-color: ${(props) =>
     props.type === "filled" ? "black" : "transparent"};
   color: ${(props) => props.type === "filled" && "white"};
+  border-radius: 8px;
 
   &:hover {
     background-color: teal;
@@ -73,6 +133,7 @@ const TopText = styled.span`
   cursor: pointer;
   font-weight: bold;
   margin: 0px 30px;
+
   &:hover {
     color: Teal;
   }
@@ -84,6 +145,64 @@ const Bottom = styled.div`
 const Info = styled.div`
   flex: 3;
 `;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetails = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span``;
+
+const PriceDetails = styled.span`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.span`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.span`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const ProductId = styled.span``;
+
+const Hr = styled.hr`
+background-color: #eee;
+border: none;
+height: 4px;
+`;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+const ProductSize = styled.span``;
+
 const Summary = styled.div`
   flex: 1;
   background-color: teal;
