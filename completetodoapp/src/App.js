2e8fe8fee455794410bf4eb55f8c78f4ff6b5d41
Vivek Goswami <101577915+vivekgoswami934@users.noreply.github.com>
2022-09-18 17:52:44 +0530

    checking

--- completetodoapp/src/App.js
+++ completetodoapp/src/App.js
@@ -1,18 +1,16 @@
-import './App.css';
-import Home from './Components/Pages/Home';
-import {Routes,Route} from "react-router-dom"
-import Content from './Components/Pages/content/Content';
+import "./App.css";
+import Home from "./Components/Pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Content from "./Components/Pages/content/Content";
 function App() {
   return (
     <div className="App">
-      
-        <Home />
+      <Home />
 
       {/* <Routes>
          <Route path="/" element={<Content />} />
          <Route path="/"  />
       </Routes> */}
-
     </div>
   );
 }
